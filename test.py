42533ab844c2c9594535aa7cfca0130da0a428e9
Dima <dima220997999@gmail.com>
2024-09-23 14:30:03 +0300

    add test script

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import aiohttp
 import asyncio
 
-# Функция для отправки POST-запроса
+# Асинхронная функция для отправки post запросов через единую сессию
 async def post_order(session, order_id):
     url = "http://localhost:3000/orders"
     json_data = {
@@ -15,7 +15,7 @@ async def post_order(session, order_id):
             "city": "Kiryat Mozkin",
             "address": "Ploshad Mira 15",
             "region": "Kraiot",
-            "email": "test@gmail.com"
+            "email": "test@gmail.com",
         },
         "payment": {
             "transaction": f"b563feb7b2b84b6test{order_id}",
@@ -27,7 +27,7 @@ async def post_order(session, order_id):
             "bank": "alpha",
             "delivery_cost": 1500,
             "goods_total": 317,
-            "custom_fee": 0
+            "custom_fee": 0,
         },
         "items": [
             {
@@ -41,7 +41,7 @@ async def post_order(session, order_id):
                 "total_price": 317,
                 "nm_id": 2389212,
                 "brand": "Vivienne Sabo",
-                "status": 202
+                "status": 202,
             }
         ],
         "locale": "en",
@@ -51,21 +51,36 @@ async def post_order(session, order_id):
         "shardkey": "9",
         "sm_id": 99,
         "date_created": "2021-11-26T06:22:19Z",
-        "oof_shard": "1"
+        "oof_shard": "1",
     }
-    
+
     async with session.post(url, json=json_data) as response:
         if response.status == 200:
             print(f"Order {order_id} posted successfully")
         else:
             print(f"Failed to post order {order_id}, status code: {response.status}")
 
-# Основная функция
-async def main():
-    async with aiohttp.ClientSession() as session:
-        post_tasks = [post_order(session, i) for i in range(1, 11)]  # Создаем 10 POST-запросов
-        await asyncio.gather(*post_tasks)
+# Асинхронная функция для отправки get запросов через единую сессию
+async def get_order(session, order_id):
+    url = f"http://localhost:3000/orders/b563feb7b2b84b6test{order_id}"
 
-# Запуск
+    async with session.get(url) as response:
+        if response.status == 200:
+            order_data = await response.json()
+            print(f"Order {order_id} data: {order_data}")
+        else:
+            print(f"Failed to get order {order_id}, status code: {response.status}")
+
+
+async def main():
+    # Открытие единой http сессии
+    async with aiohttp.ClientSession() as session:
+        post_tasks = [post_order(session, i) for i in range(1, 11)]
+        await asyncio.gather(*post_tasks)
+        # Использование gather для параллельного запуска всех задач
+        get_tasks = [get_order(session, i) for i in range(1, 11)]
+        await asyncio.gather(*get_tasks)
+
+
 if __name__ == "__main__":
     asyncio.run(main())
